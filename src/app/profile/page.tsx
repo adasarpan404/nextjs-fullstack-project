5f28fe4a5dd9c59408899d4d5c4022ec31cea05b
Arpan Das <dasarpan471@gmail.com>
2023-12-09 17:17:44 +0530

    basic learning about nextjs done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,12 @@
 import axios from 'axios'
 import toast from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function ProfilePage() {
     const router = useRouter();
+    const [data, setData] = useState("Nothing");
     const logout = async () => {
         try {
             await axios.get('/api/user/logout')
@@ -15,14 +18,21 @@ export default function ProfilePage() {
             toast.error(error.message)
         }
     }
+    const getUserProfile = async () => {
+        const res = await axios.get('/api/user/me')
+        console.log(res)
+        setData(res.data.data)
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile Page</p>
-
+            <h2 className="p-1 rounded bg-green-500">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}
+            </Link>}</h2>
             <hr />
             <button onClick={logout} className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">Logout</button>
+            <button onClick={getUserProfile} className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"></button>
         </div>
     )
 }
